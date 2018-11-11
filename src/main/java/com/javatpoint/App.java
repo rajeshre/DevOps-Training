a993971981ba6becd253a93e478bdfe605555eb9
rajeshre <rajeshre@gmail.com>
2018-11-11 12:00:09 -0500

    Update App.java

--- src/main/java/com/javatpoint/App.java
+++ src/main/java/com/javatpoint/App.java
@@ -10,6 +10,6 @@ public class App
     
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!"+hello.toString() );
+        System.out.println( "Hello World!"+hello );
     }
 }
